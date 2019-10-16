90fe1221091b5c4530382db1d2870b268893c675
Marek Kazana <m.kazana@interia.pl>
2019-10-16 23:29:11 +0200

    Creating connection with database

---
+++ src/main/java/pl/marekkazana/imguploadview/ImgUploadViewApplication.java
@@ -0,0 +1,13 @@
+package pl.marekkazana.imguploadview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgUploadViewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImgUploadViewApplication.class, args);
+    }
+
+}
